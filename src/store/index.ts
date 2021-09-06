23e34f5be13babdf3eba04344cbaad3f15eb7fb6
pvl <pvlchkhrv@gmail.com>
2021-09-06 17:53:05 +0300

    init: calendar

---
+++ src/store/index.ts
@@ -0,0 +1,15 @@
+import {applyMiddleware, combineReducers, createStore} from 'redux';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({
+
+})
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export {
+    store
+}
